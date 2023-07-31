1cf4551e4d8c8cf59482d0be46f1fc6ea4fb1f35
liaoallen <f110143103@nkust.edu.tw>
2023-07-31 15:17:04 +0800

    add function get_max_maches_for_win_num

--- src/invoice.py
+++ src/invoice.py
@@ -1,14 +1,18 @@
 winning_number=['94899145','71143793','41055355']
 # Unit test
-def get_last_n_digit(ticket,n):
+def get_last_n_digits(ticket,n):
     #假設給定一組號碼，回傳最後n碼，
     #例如get_last_n_digit('94899145',3)會回傳'145'
     #例如get_last_n_digit('94899145',5)會回傳'99145'
     return ticket[-n:]
 
-if __name__=='__main__':
-    ticket='94899145'
-    last_4_digit=get_last_n_digit(ticket,4)
-    print(last_4_digit)
-    assert last_4_digit=='9145'
-# 主要用來檢查程式碼是否有誤，若有誤則會報錯(利用assert回報錯誤，正確則不會回報錯誤)
+def get_max_match_for_win_mun(ticket,win_num):
+    match_count = 0 #最大相同數字個數
+    for i in range(3,9):
+        t = get_last_n_digits(ticket,i)
+        w = get_last_n_digits(win_num,i)
+        if t==w:
+            match_count=i
+        else:
+            break
+    return match_count
--- testcases/test_invoice.py
+++ testcases/test_invoice.py
@@ -1,4 +1,4 @@
-from src.invoice import get_last_n_digit
+from src.invoice import get_last_n_digit,get_max_match_for_win_mun
 
 def test_get_last_3_digit():
     ticket='94899145'
@@ -9,3 +9,21 @@ def test_get_last_8_digit():
     ticket='94899145'
     last_8_digit=get_last_n_digit(ticket,8)
     assert last_8_digit=='94899145'
+
+def test_max_3_match():
+    ticket = '94899145'
+    win_num = '94898145'
+    max_match = get_max_match_for_win_mun(ticket,win_num)
+    assert max_match == 3
+
+def test_no_match():
+    ticket = '94899045'
+    win_num = '94898145'
+    max_match = get_max_match_for_win_mun(ticket,win_num)
+    assert max_match == 0
+
+def test_max_8_match():
+    ticket = '94898145'
+    win_num = '94898145'
+    max_match = get_max_match_for_win_mun(ticket,win_num)
+    assert max_match == 8  
