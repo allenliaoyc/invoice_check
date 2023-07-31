ba2485b80d02670b7f204a7daf8c906be722afe1
liaoallen <f110143103@nkust.edu.tw>
2023-07-31 15:22:46 +0800

    重新上傳

--- testcases/test_invoice.py
+++ testcases/test_invoice.py
@@ -1,13 +1,13 @@
-from src.invoice import get_last_n_digit,get_max_match_for_win_mun
+from src.invoice import get_last_n_digits,get_max_match_for_win_mun
 
 def test_get_last_3_digit():
     ticket='94899145'
-    last_3_digit=get_last_n_digit(ticket,3)
+    last_3_digit=get_last_n_digits(ticket,3)
     assert last_3_digit=='145'
 
 def test_get_last_8_digit():
     ticket='94899145'
-    last_8_digit=get_last_n_digit(ticket,8)
+    last_8_digit=get_last_n_digits(ticket,8)
     assert last_8_digit=='94899145'
 
 def test_max_3_match():
